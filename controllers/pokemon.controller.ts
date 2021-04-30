094ebd31f869040082386a07741af93e5cbc1c9f
Teddy <jfrancoisteddy@gmail.com>
2021-04-30 15:16:44 +0200

    Try setTimeout

--- controllers/pokemon.controller.ts
+++ controllers/pokemon.controller.ts
@@ -1,5 +1,14 @@
 const chalk = require('chalk');
 
+export interface IPokemon {
+    name: string;
+    hp: number
+    level: number;
+    base_power: number;
+    offensive_stat: number;
+    defensive_stat: number;
+}
+
 export class Pokemon {
     name: string;
     hp: number
@@ -8,39 +17,79 @@ export class Pokemon {
     offensive_stat: number;
     defensive_stat: number;
 
-    constructor(name: string, hp: number, level: number, base_power: number, offensive_stat: number, defensive_stat: number){
-        this.name = name;
-        this.hp = hp;
-        this.level = level;
-        this.base_power = base_power;
-        this.offensive_stat = offensive_stat;
-        this.defensive_stat = defensive_stat;
+    constructor(props:IPokemon){
+        this.name = props.name;
+        this.hp = props.hp;
+        this.level = props.level;
+        this.base_power = props.base_power;
+        this.offensive_stat = props.offensive_stat;
+        this.defensive_stat = props.defensive_stat;
     }
 
     public attack(adversary: Pokemon): number {
         let damage = Math.floor(Math.floor(Math.floor(2 * this.level / 5 + 2) * this.offensive_stat * this.base_power / this.defensive_stat) / 50) + 2;
-        return adversary.hp - damage;
+        return adversary.hp -= damage;
     }
 
-    public fight(adversary: Pokemon): Pokemon {
-        let i = 0;
-        while(this.hp > 0 && adversary.hp > 0) {
-            if(i % 2 == 0){
-                adversary.hp = this.attack(adversary);
-                //console.log(chalk.red(adversary.name + " pokemon has " + adversary.hp + " hp left"));
-            }else{
-                this.hp = adversary.attack(this);
-                //console.log(chalk.blue(this.name + " pokemon has " + this.hp + " hp left"));
+    /**
+     *
+     * Function without setTimeout
+     *
+     * @param adversary
+     */
+    public fight(adversary: Pokemon): Promise<Pokemon> {
+        return new Promise<Pokemon>(async (resolve, reject) => {
+            let i = 0;
+
+            while(this.hp > 0 && adversary.hp > 0) {
+                if(i % 2 == 0){
+                    adversary.hp = this.attack(adversary);
+                    //console.log(chalk.red(adversary.name + " pokemon has " + adversary.hp + " hp left"));
+                }else {
+                    this.hp = adversary.attack(this);
+                    //console.log(chalk.blue(this.name + " pokemon has " + this.hp + " hp left"));
+                }
+                i++;
             }
 
-            i++;
-        }
+            if(this.hp <= 0)
+                resolve(adversary);
+            else
+                resolve(this);
+        })
+    }
 
-        if(this.hp <= 0)
-            return adversary;
-        else
-            return this;
+    /**
+     *
+     * Function with setTimeout
+     *
+     * I can't put a while with a settimeout despite 2hours of searching
+     *
+     * @param adversary
+     */
+    public fightTimeout(adversary: Pokemon): Promise<Pokemon> {
+        return new Promise<Pokemon>(async (resolve, reject) => {
+            let i = 0;
 
+            while(this.hp > 0 && adversary.hp > 0) {
+                ((i) => {
+                    setTimeout(() => {
+                        if (i % 2 == 0) {
+                            adversary.hp = this.attack(adversary);
+                            //console.log(chalk.red(adversary.name + " pokemon has " + adversary.hp + " hp left"));
+                        } else {
+                            this.hp = adversary.attack(this);
+                            //console.log(chalk.blue(this.name + " pokemon has " + this.hp + " hp left"));
+                        }
+                    }, 1000);
+                })(i++)
+            }
+
+            if(this.hp <= 0)
+                resolve(adversary);
+            else
+                resolve(this);
+        })
     }
 
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,22 @@
 import {Pokemon} from './controllers';
 
-const PokeA = new Pokemon("Scorplack", 65, 2, 1, 100, 50);
-const PokeB = new Pokemon("Polaryu", 85, 2, 1, 75, 90);
+const PokeA = new Pokemon({
+    name: "Polaryu",
+    hp: 85,
+    level: 2,
+    base_power: 1,
+    offensive_stat: 75,
+    defensive_stat: 90
+})
+
+const PokeB = new Pokemon({
+    name: "Scorplack",
+    hp: 65,
+    level: 2,
+    base_power: 1,
+    offensive_stat: 100,
+    defensive_stat: 50
+})
+
 
 console.log( PokeA.fight(PokeB) );
